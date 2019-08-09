283986ab434c248c4cae3b28a9e403e72ec0fc58
kvaps <kvapss@gmail.com>
2019-08-09 16:48:34 +0200

    fix: build image tag

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -3,5 +3,5 @@
 cd $(dirname $0)
 
 for image in $(find . -maxdepth 2 -name Dockerfile | awk -F/ '{print $2}'); do
-  docker build --build-arg VERSION=${1-latest} -t ${CI_REGISTRY_IMAGE:-kvaps}/$image:${CI_COMMIT_REF_NAME:-latest} $image
+  docker build --build-arg VERSION=${CI_COMMIT_REF_NAME-latest} -t ${CI_REGISTRY_IMAGE:-kvaps}/$image:${CI_COMMIT_REF_NAME:-latest} $image
 done

4c4a8623e07ff0eec861be53b3c9d51bb54d9b92
Andrei Kvapil <kvapss@gmail.com>
2020-05-15 09:54:31 +0200

    bump version v1.7.1

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -4,5 +4,5 @@ set -e
 cd $(dirname $0)
 
 for image in $(find . -maxdepth 2 -name Dockerfile | awk -F/ '{print $2}'); do
-  docker build --build-arg VERSION=${CI_COMMIT_REF_NAME-latest} -t ${CI_REGISTRY_IMAGE:-kvaps}/$image:${CI_COMMIT_REF_NAME:-latest} $image
+  docker build -t ${CI_REGISTRY_IMAGE:-kvaps}/$image:${CI_COMMIT_REF_NAME:-latest} $image
 done
